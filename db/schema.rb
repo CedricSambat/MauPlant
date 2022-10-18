bbffc746edac958f9fce7a97c9acb755c669ded1
Ashley Vengatasamy <vengatasamy.ashley@gmail.com>
2022-10-18 13:48:38 +0400

    Plat index, show and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_064618) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_072845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_064618) do
   end
 
   create_table "plants", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.text "description"
     t.string "category"
     t.datetime "created_at", null: false
