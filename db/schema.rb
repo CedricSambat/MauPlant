960ec08e8e33b098f7a7ecca58cd99dab0ceea1c
cedric sambat <cedricsambat9818@gmail.com>
2022-10-19 11:22:01 +0400

    add_name_to_garden

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_072845) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_071840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_072845) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
