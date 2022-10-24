9934e61eca058f35968e1aba40578963a90aca9e
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-24 20:53:34 +0400

    add modal.scss for modal design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @gardens = current_user.gardens
 
-    #  @current_user = User.find(garden_params([:id]))
+    # @gardens = current_user.gardens
+
+    # @current_user = User.find(garden_params([:id])
     #  @gardens = current_user.garden_plants
-    # @gardens = Garden.all
-    # @current_user = User.new
+    @gardens = Garden.all.where(user_id: current_user)
+    
     # if current_user.gardens
     #   @gardens = Garden.all.where(user_id: current_user)
     # else
