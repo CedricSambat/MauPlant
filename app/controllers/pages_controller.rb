7e613e4bc0e22d168c344624bcf45ad977a69c00
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-24 15:45:58 +0400

    modal completed..front remaining

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @garden = Garden.all
+    @current_user = User.new
+    if current_user.gardens
+      @gardens = Garden.all.where(user_id: current_user)
+    else
+      redirect_to "/gardens/new"
+    end
   end
 end
