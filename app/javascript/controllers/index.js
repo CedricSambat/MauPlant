97be2b734836903d73c72250cf044aa563945e7b
Ameenah Rostum <ameenah141991@gmail.com>
2022-10-25 12:06:53 +0400

    modal functioning.displaying message for reminder

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("chatroom-subscription", ChatroomSubscriptionController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ReminderController from "./reminder_controller"
+application.register("reminder", ReminderController)
