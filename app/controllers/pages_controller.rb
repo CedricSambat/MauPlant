5e41daff5357d21d4b8b15de56dec23cdf5576de
Komal Narrainen <komal.narrainen93@gmail.com>
2022-10-26 16:22:00 +0400

    modify chatroom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-
+    @chatroom = Chatroom.last
+  
     # @gardens = current_user.gardens
 
     # @current_user = User.find(garden_params([:id])
